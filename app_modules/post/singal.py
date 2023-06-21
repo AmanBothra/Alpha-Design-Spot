68ee701dfafe4d5932e8f7b72b9d2f1da8588ac1
aman-eminence <aman@eminenceinfotech.com>
2023-06-21 19:24:50 +0530

    fix signal issue

--- app_modules/post/singal.py
+++ app_modules/post/singal.py
@@ -26,6 +26,7 @@ def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
         
 @receiver(post_save, sender=OtherPost)
 def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
+    
     customer_group = instance.group
     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
 
@@ -33,7 +34,7 @@ def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
     for customer_id in customer_ids:
         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
         for customer_frame in customer_frames:
-            mapping = CustomerPostFrameMapping.objects.create(
+            mapping = CustomerOtherPostFrameMapping.objects.create(
                 customer_id=customer_id,
                 other_post=instance,
                 customer_frame=customer_frame,
