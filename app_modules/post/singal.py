4a8ee31ffe1a0c14cfad93fbe2966b37d0dbab22
Aman Bothra <aman@eminenceinfotech.com>
2024-02-07 11:57:44 +0530

    modify post model signal

--- app_modules/post/singal.py
+++ app_modules/post/singal.py
@@ -1,30 +1,47 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-
+from django.db import transaction
 from .models import (
     Post, CustomerPostFrameMapping, CustomerOtherPostFrameMapping, OtherPost, BusinessPost,
     BusinessPostFrameMapping
 )
 
 
+# @receiver(post_save, sender=Post)
+# def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
+#     customer_group = instance.group
+#     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
+#
+#     for customer_id in customer_ids:
+#         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
+#         for customer_frame in customer_frames:
+#             mapping, created = CustomerPostFrameMapping.objects.get_or_create(
+#                 customer_id=customer_id,
+#                 post=instance,
+#                 customer_frame=customer_frame
+#             )
+#             mapping.save()
+
+
 @receiver(post_save, sender=Post)
 def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
-    customer_group = instance.group
-    customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
+    if not created:
+        return
 
-    for customer_id in customer_ids:
-        customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
-        for customer_frame in customer_frames:
-            mapping, created = CustomerPostFrameMapping.objects.get_or_create(
-                customer_id=customer_id,
-                post=instance,
-                customer_frame=customer_frame
-            )
-            mapping.is_downloaded = False
-            mapping.save()
-            
-            
+    customer_group = instance.group
+    customer_frames = customer_group.customer_frame_group.select_related('customer_frame').all()
+
+    customer_frame_mappings = []
+    for customer_frame in customer_frames:
+        customer_frame_mappings.append(CustomerPostFrameMapping(
+            customer_id=customer_frame.customer_id,
+            post=instance,
+            customer_frame=customer_frame,
+        ))
+
+    with transaction.atomic():
+        CustomerPostFrameMapping.objects.bulk_create(customer_frame_mappings)
+
 
 @receiver(post_save, sender=OtherPost)
 def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
@@ -42,9 +59,7 @@ def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
             mapping.is_downloaded = False
             mapping.save()
 
- 
- 
-    
+
 @receiver(post_save, sender=BusinessPost)
 def mapping_business_post_with_customer_frame(sender, instance, created, **kwargs):
     customer_group = instance.group
