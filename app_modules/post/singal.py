223a1be8253eb539737935250e0f3d3340cc07de
Aman Bothra <aman@eminenceinfotech.com>
2024-02-07 19:25:31 +0530

    modify post model signal

--- app_modules/post/singal.py
+++ app_modules/post/singal.py
@@ -7,40 +7,40 @@ from .models import (
 )
 
 
-# @receiver(post_save, sender=Post)
-# def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
-#     customer_group = instance.group
-#     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
-#
-#     for customer_id in customer_ids:
-#         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
-#         for customer_frame in customer_frames:
-#             mapping, created = CustomerPostFrameMapping.objects.get_or_create(
-#                 customer_id=customer_id,
-#                 post=instance,
-#                 customer_frame=customer_frame
-#             )
-#             mapping.save()
-
-
 @receiver(post_save, sender=Post)
 def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
-    if not created:
-        return
-
     customer_group = instance.group
-    customer_frames = customer_group.customer_frame_group.select_related('customer_frame').all()
+    customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
 
-    customer_frame_mappings = []
-    for customer_frame in customer_frames:
-        customer_frame_mappings.append(CustomerPostFrameMapping(
-            customer_id=customer_frame.customer_id,
-            post=instance,
-            customer_frame=customer_frame,
-        ))
+    for customer_id in customer_ids:
+        customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
+        for customer_frame in customer_frames:
+            mapping, created = CustomerPostFrameMapping.objects.get_or_create(
+                customer_id=customer_id,
+                post=instance,
+                customer_frame=customer_frame
+            )
+            mapping.save()
 
-    with transaction.atomic():
-        CustomerPostFrameMapping.objects.bulk_create(customer_frame_mappings)
+
+# @receiver(post_save, sender=Post)
+# def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
+#     if not created:
+#         return
+#
+#     customer_group = instance.group
+#     customer_frames = customer_group.customer_frame_group.select_related('customer_frame').all()
+#
+#     customer_frame_mappings = []
+#     for customer_frame in customer_frames:
+#         customer_frame_mappings.append(CustomerPostFrameMapping(
+#             customer_id=customer_frame.customer_id,
+#             post=instance,
+#             customer_frame=customer_frame,
+#         ))
+#
+#     with transaction.atomic():
+#         CustomerPostFrameMapping.objects.bulk_create(customer_frame_mappings)
 
 
 @receiver(post_save, sender=OtherPost)
