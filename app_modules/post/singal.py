32178e7f049ed0c1284d112a9bf4754574950067
Aman Bothra <aman@eminenceinfotech.com>
2024-03-13 13:28:05 +0530

    update signal code

--- app_modules/post/singal.py
+++ app_modules/post/singal.py
@@ -43,22 +43,6 @@ def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
         CustomerPostFrameMapping.objects.bulk_create(customer_frame_mappings)
 
 
-# @receiver(post_save, sender=OtherPost)
-# def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
-#     customer_group = instance.group
-#     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
-#
-#     for customer_id in customer_ids:
-#         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
-#         for customer_frame in customer_frames:
-#             mapping, created = CustomerOtherPostFrameMapping.objects.get_or_create(
-#                 customer_id=customer_id,
-#                 other_post=instance,
-#                 customer_frame=customer_frame
-#             )
-#             mapping.is_downloaded = False
-#             mapping.save()
-
 @receiver(post_save, sender=OtherPost)
 def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
     customer_group = instance.group
@@ -66,24 +50,40 @@ def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
 
     for customer_id in customer_ids:
         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
-
-        mappings_to_create = []
         for customer_frame in customer_frames:
             mapping, created = CustomerOtherPostFrameMapping.objects.get_or_create(
                 customer_id=customer_id,
                 other_post=instance,
-                customer_frame=customer_frame,
-                defaults={'is_downloaded': False}  # Set default values
+                customer_frame=customer_frame
             )
-            if not created:
-                # Update existing mapping
-                mapping.is_downloaded = False
-                mapping.save()
-            else:
-                mappings_to_create.append(mapping)
+            mapping.is_downloaded = False
+            mapping.save()
 
-        # Bulk create mappings for the current customer_id
-        CustomerOtherPostFrameMapping.objects.bulk_create(mappings_to_create)
+# @receiver(post_save, sender=OtherPost)
+# def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
+#     customer_group = instance.group
+#     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
+#
+#     for customer_id in customer_ids:
+#         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
+#
+#         mappings_to_create = []
+#         for customer_frame in customer_frames:
+#             mapping, created = CustomerOtherPostFrameMapping.objects.get_or_create(
+#                 customer_id=customer_id,
+#                 other_post=instance,
+#                 customer_frame=customer_frame,
+#                 defaults={'is_downloaded': False}
+#             )
+#             if not created:
+#                 # Update existing mapping
+#                 mapping.is_downloaded = False
+#                 mapping.save()
+#             else:
+#                 mappings_to_create.append(mapping)
+#
+#         # Bulk create mappings for the current customer_id
+#         CustomerOtherPostFrameMapping.objects.bulk_create(mappings_to_create)
 
 
 # @receiver(post_save, sender=BusinessPost)
