79e9a56deda6e21bccced3e5b0d2f9fffed0d6e4
AmanBothra <amanbothra1777@gmail.com>
2024-07-20 20:37:06 +0530

    update post signal for other post

--- app_modules/post/singal.py
+++ app_modules/post/singal.py
@@ -6,22 +6,10 @@ from .models import (
     Post, CustomerPostFrameMapping, CustomerOtherPostFrameMapping, OtherPost, BusinessPost,
     BusinessPostFrameMapping
 )
+from django.db.models import Q
+from django.db.utils import IntegrityError
 
 
-# @receiver(post_save, sender=Post)
-# def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
-#     customer_group = instance.group
-#     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
-#
-#     for customer_id in customer_ids:
-#         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
-#         for customer_frame in customer_frames:
-#             mapping, created = CustomerPostFrameMapping.objects.get_or_create(
-#                 customer_id=customer_id,
-#                 post=instance,
-#                 customer_frame=customer_frame
-#             )
-#             mapping.save()
 
 
 @receiver(post_save, sender=Post)
@@ -43,59 +31,59 @@ def mapping_post_with_customer_frame(sender, instance, created, **kwargs):
     with transaction.atomic():
         CustomerPostFrameMapping.objects.bulk_create(customer_frame_mappings)
 
+@receiver(post_save, sender=OtherPost)
+def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
+    if created:
+        customer_group = instance.group
+        customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
+
+        for customer_id in customer_ids:
+            customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
+
+            mappings_to_create = []
+            for customer_frame in customer_frames:
+                # Check for existing mapping before adding to the list
+                if not CustomerOtherPostFrameMapping.objects.filter(
+                    Q(customer_id=customer_id) & 
+                    Q(other_post=instance) & 
+                    Q(customer_frame=customer_frame)
+                ).exists():
+                    mappings_to_create.append(CustomerOtherPostFrameMapping(
+                        customer_id=customer_id,
+                        other_post=instance,
+                        customer_frame=customer_frame,
+                    ))
+
+            # Bulk create mappings for the current customer_id
+            if mappings_to_create:
+                with transaction.atomic():
+                    try:
+                        CustomerOtherPostFrameMapping.objects.bulk_create(mappings_to_create)
+                    except IntegrityError:
+                        # Handle the case where a mapping might have been created by another process
+                        pass
 
 # @receiver(post_save, sender=OtherPost)
 # def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
 #     customer_group = instance.group
 #     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
-#
+
 #     for customer_id in customer_ids:
 #         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
+
+#         mappings_to_create = []
 #         for customer_frame in customer_frames:
-#             mapping, created = CustomerOtherPostFrameMapping.objects.get_or_create(
+#             mappings_to_create.append(CustomerOtherPostFrameMapping(
 #                 customer_id=customer_id,
 #                 other_post=instance,
-#                 customer_frame=customer_frame
-#             )
-#             mapping.is_downloaded = False
-#             mapping.save()
+#                 customer_frame=customer_frame,
+#             ))
 
-@receiver(post_save, sender=OtherPost)
-def mapping_other_post_with_customer_frame(sender, instance, created, **kwargs):
-    customer_group = instance.group
-    customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
-
-    for customer_id in customer_ids:
-        customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
-
-        mappings_to_create = []
-        for customer_frame in customer_frames:
-            mappings_to_create.append(CustomerOtherPostFrameMapping(
-                customer_id=customer_id,
-                other_post=instance,
-                customer_frame=customer_frame,
-            ))
-
-        # Bulk create mappings for the current customer_id
-        with transaction.atomic():
-            CustomerOtherPostFrameMapping.objects.bulk_create(mappings_to_create)
+#         # Bulk create mappings for the current customer_id
+#         with transaction.atomic():
+#             CustomerOtherPostFrameMapping.objects.bulk_create(mappings_to_create)
 
 
-# @receiver(post_save, sender=BusinessPost)
-# def mapping_business_post_with_customer_frame(sender, instance, created, **kwargs):
-#     customer_group = instance.group
-#     customer_ids = customer_group.customer_frame_group.values_list('customer_id', flat=True)
-#
-#     for customer_id in customer_ids:
-#         customer_frames = customer_group.customer_frame_group.filter(customer_id=customer_id)
-#         for customer_frame in customer_frames:
-#             mapping, created = BusinessPostFrameMapping.objects.get_or_create(
-#                 customer_id=customer_id,
-#                 post=instance,
-#                 customer_frame=customer_frame
-#             )
-#             mapping.is_downloaded = False
-#             mapping.save()
 
 @receiver(post_save, sender=BusinessPost)
 def mapping_business_post_with_customer_frame(sender, instance, created, **kwargs):
