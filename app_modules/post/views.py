3d3fe0213b65180c4c943422982509c862865939
AmanBothra <amanbothra1777@gmail.com>
2024-10-21 18:03:29 +0530

    modify business post api

--- app_modules/post/views.py
+++ app_modules/post/views.py
@@ -181,6 +181,17 @@ class BusinessPostViewset(viewsets.ModelViewSet):
             queryset = queryset.filter(file_type=file_type)
 
         return queryset
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response({'results': serializer.data})
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'results': serializer.data})
 
 class CustomerPostFrameMappingViewSet(BaseModelViewSet):
     queryset = CustomerPostFrameMapping.objects
