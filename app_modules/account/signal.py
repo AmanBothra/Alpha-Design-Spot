589c877394f7150fa735276a118f873db6a6a199
Aman <amanbothra1777@gmail.com>
2024-12-03 05:39:09 +0000

    update code

--- app_modules/account/signal.py
+++ app_modules/account/signal.py
@@ -50,8 +50,8 @@ from .models import CustomerFrame
 @receiver(post_save, sender=CustomerFrame)
 def trigger_mapping_post_task(sender, instance, created, **kwargs):
     if created:
-        mapping_customer_frame_with_post.delay(instance.id)
-        # mapping_customer_frame_with_post(instance.id)
+        #mapping_customer_frame_with_post.delay(instance.id)
+         mapping_customer_frame_with_post(instance.id)
         
 
 
@@ -95,8 +95,8 @@ def trigger_mapping_post_task(sender, instance, created, **kwargs):
 @receiver(post_save, sender=CustomerFrame)
 def trigger_mapping_task(sender, instance, created, **kwargs):
     if created:
-        mapping_customer_frame_with_other_posts.delay(instance.id)
-        # mapping_customer_frame_with_other_posts(instance.id)
+        # mapping_customer_frame_with_other_posts.delay(instance.id)
+         mapping_customer_frame_with_other_posts(instance.id)
         
 
 
@@ -123,6 +123,6 @@ def trigger_mapping_task(sender, instance, created, **kwargs):
 @receiver(post_save, sender=CustomerFrame)
 def trigger_customer_frame_mapping(sender, instance, created, **kwargs):
     if created:
-        map_customer_frame_with_business_posts.delay(instance.id)
-        # map_customer_frame_with_business_posts(instance.id)
+        # map_customer_frame_with_business_posts.delay(instance.id)
+        map_customer_frame_with_business_posts(instance.id)
         
